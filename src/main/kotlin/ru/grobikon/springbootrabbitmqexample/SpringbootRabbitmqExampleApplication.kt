beb31902df62851f42baf874460f4919ed4efc5d
Образцов Григорий <grobikon@mail.ru>
2021-12-07 02:28:22 +0300

    Create project.

---
+++ src/main/kotlin/ru/grobikon/springbootrabbitmqexample/SpringbootRabbitmqExampleApplication.kt
@@ -0,0 +1,11 @@
+package ru.grobikon.springbootrabbitmqexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootRabbitmqExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootRabbitmqExampleApplication>(*args)
+}
---
+++ src/test/kotlin/ru/grobikon/springbootrabbitmqexample/SpringbootRabbitmqExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.grobikon.springbootrabbitmqexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootRabbitmqExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
